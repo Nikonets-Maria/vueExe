35d47d58c985846fdeced51b0b593dbb6f71fad4
Nikonets-Maria <maria.nikonec@gmail.com>
2025-01-29 21:36:40 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+
+
+<template>
+ 
+  <div class="main_content">
+    <Form></Form>
+  </div>
+
+</template>
+
+<script >
+import Form from './components/Form.vue';
+
+export default {
+  components: {
+    Form
+  },
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main_content{
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+}
+
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,260 @@
+
+
+
+<template>
+  <div class="main_content">
+  <div class="card">
+    <div class="form_content">
+      <form class="under_form">
+        <hr className="line"></hr>
+        <input v-model="formData.cardCVC" type="number" placeholder="CVC" class="cvc_input"/>
+      </form> 
+
+      <form @submit.prevent="addUserData()" class="card_form">
+      
+            <input v-model="formData.cardHolder" type="text" placeholder="Holder of card"/>
+            <input v-model="formData.cardNumber" type="number" placeholder="Number of card"/>      
+            <label class="valid">VALID THRU</label>
+            <label class="card_content">
+              <input v-model="formData.cardMonth" type="number" placeholder="MM" class="num_input"/>
+              <label>/</label>
+              <input v-model="formData.cardYear" type="number" placeholder="YY" class="num_input"/>
+          </label>
+          <input type="submit" value="Send" class="submit_btn"/>
+
+      </form>
+      <img :src="cardImg" class="card_logo"/>
+
+    </div>
+ 
+
+
+    <img :src="cardLogo" class="logo"/>
+
+  </div>
+    
+
+    <div class="table_contain">
+
+        <table class="card_table">
+          <thead class="table_head">
+            <tr>
+              <th><h4>NAME</h4></th>
+              <th><h4>CARD NUMBER</h4></th>
+              <th><h4>DATE EXPIRE</h4></th>
+              <th><h4>CODE</h4></th>
+            </tr>
+          </thead>
+          <tbody class="table_bdy">
+            <tr v-for="item in usersData" :key="item.id" > 
+              <td class="table_line">{{ item.name }}{{ item.firstName }}</td>
+              <td class="table_line">{{ item.cardNumber }}</td>
+              <td class="table_line">{{ item.dateExpire }}</td>
+              <td class="table_line">{{ item.code }}</td>
+            </tr>
+          </tbody>
+      </table>
+
+    </div>
+
+  </div>
+
+
+
+</template>
+
+<script >
+import cardLogo from "../assets/images/logo.png"
+import cardImg from "../assets/images/cardImg.png"
+
+export default {
+
+  data(){
+    return {
+      cardLogo: cardLogo,
+      cardImg: cardImg,
+      formData:{
+        cardHolder: "",//"Holder of card", 2 words
+        cardNumber: null,//"Number of card", 16 num
+        cardMonth: null,//"MM",  1-12
+        cardYear: null,//"YY",  date? до "сегодня"
+        cardCVC: null//"CVC",  *** 
+      },
+
+      usersData: [
+        {id: 1, name: 'Jane', firstName: 'Doe', cardNumber: 1234123412341234, dateExpire: 110121, code: 123},
+        // {id: 2, name: 'John', firstName: 'Doe', cardNumber: 1234123412341234, dateExpire: 110121, code: 123},
+        // {id: 3, name: 'John', firstName: 'Doe', cardNumber: 1234123412341234, dateExpire: 110121, code: 123},
+        // {id: 5, name: 'John', firstName: 'Doe', cardNumber: 1234123412341234, dateExpire: 110121, code: 123},
+        // {id: 6, name: 'John', firstName: 'Doe', cardNumber: 1234123412341234, dateExpire: 110121, code: 123},
+        // {id: 7, name: 'John', firstName: 'Doe', cardNumber: 1234123412341234, dateExpire: 110121, code: 123},
+
+      ]
+      
+    }
+  },
+
+  methods:{
+    addUserData(){
+      this.usersData.push({
+        id: Date.now,
+        name: 'John', 
+        firstName: 'Doe', 
+        cardNumber: 1234123412341234, 
+        dateExpire: 110121, 
+        code: 123
+      })
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+.table_contain{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 28px;
+    width: 810px;
+    background-color: #95B0D8;
+    padding: 20px;
+    margin-bottom: 30%;
+  }
+
+  .card_table{
+    width: 680px;
+  }
+
+
+  .table_head{
+    background-color: #E5E5E5;
+    color: #000;
+  }
+
+  .table_bdy{
+    background-color: #FFFFFF;
+  }
+
+  .table_line{
+    width: 170px;
+    height: 40px;
+    padding: 5px;
+  }
+
+
+  .main_content{
+    background-color: #FFFFFF;
+    width: 1000px;
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+  }
+
+  .card{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .card_form{
+    display: flex;
+    flex-direction: column;
+    justify-content:space-between;
+    position: relative;
+    top: -160px;
+    right: 40px;
+    padding: 20px;
+    width: 388px;
+    height: 239px;
+    border-radius: 10px;
+    background-color: #ECECEC;
+    border-bottom: 3px solid rgb(177, 177, 177);
+  }
+
+  .card_content{
+    display: flex;
+    align-items: center;
+  }
+
+  .valid{
+    margin-left: 10px;
+  }
+
+  input{
+    color: #A0A0A0;
+    width: 324px;
+    height: 42px;
+    margin-top: 105px;
+    margin: 10px;
+    border: 1px solid rgb(177, 177, 177);
+    padding: 10px;
+
+  }
+
+  .num_input{
+    width: 63px;
+    height: 25px;
+  }
+
+  .cvc_input{
+    position: relative;
+    top: 100px;
+    left: 280px;
+    width: 63px;
+    height: 25px;
+  }
+
+  .submit_btn{
+    position: relative;
+    top: 120px;
+    right: 30px;
+
+    color: #FFFFFF;
+    background-color: #95B0D8;
+    border: none;
+    border-radius: 11px;
+    width: 187px;
+    height: 48px;
+    margin: 5px;
+
+  }
+
+  .under_form{
+    position: relative;
+    top: 140px;
+    left: 130px;
+
+    width: 388px;
+    height: 239px;
+    border-radius: 10px;
+    background-color: #C7C7C7;
+
+  }
+
+  .line{
+    position: relative;
+    top: 40px;
+    background-color: #979797;
+    height: 50px;
+    margin-top: 40px;
+  }
+
+  .card_logo{
+    position: relative;
+    top: -282px;
+    left: 200px;
+
+    width: 150px;
+  }
+  
+  .logo{
+    height: 120px;
+    margin-left: 120px;
+    margin-right: -10px;
+    margin-bottom: 50px;
+  }
+
+</style>
